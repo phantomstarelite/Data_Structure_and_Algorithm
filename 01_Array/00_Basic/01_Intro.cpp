e1306c8aba350d9ede1917546a11d253185d46fc
phantomstarelite <pratikkhumkar2004@gmail.com>
2025-09-18 15:22:32 +0530

    update the files

---
+++ 01_Array/00_Basic/01_Intro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "My name is pratik" << endl;
+    int a = 4;
+    cout << a << endl;
+    cout << "He is the good guy";
+    return 0;
+}
